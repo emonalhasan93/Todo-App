c1f5f6a856733a620bb96f076290abb122d2b086
emon al hasan <emonalhasan41@gmail.com>
2023-03-12 20:27:11 +0600

    add delete complete

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../components/Todolist.css'
 import todoicon from '../images/icons8-todo-list-64.png'
 import victory from '../images/peace.png'
 
 const TodoList = () => {
+    const[inputData,setInputData]=useState('');
+    const[items,setItems]=useState([]);
+    const addItem=()=>{
+        if(!inputData)
+        {
+alert("Please fill the data")
+        }else{
+            setItems([...items,inputData])
+            setInputData("")
+        }
+        
+    }
+    const deleteItem=(id)=>{
+        const updatedData=items.filter((data,indx)=>{
+            return indx!=id;
+        })
+        setItems(updatedData)
+
+    }
     return (
         <>
             <div className='main-div'>
@@ -13,16 +32,27 @@ const TodoList = () => {
                         <h3>Add Your List Here</h3>
                     </figure>
                     <div className='addItems'>
-                        <input className='input-text' type="text" placeholder='Add Items ' />
+                        <input 
+                        value={inputData}
+                        onChange={(e)=> setInputData(e.target.value)}
+                        className='input-text' type="text" placeholder='Add Items ' />
 
-                        <i className="fa fa-plus add-btn "></i>
+                        <i className="fa fa-plus add-btn " onClick={addItem}></i>
 
                     </div>
                     <div className='showItems'>
-                        <div className='eachItem'>
-                            <h4 className='heading'>Apple</h4>
-                            <i className="fa fa-trash del-btn"></i>
-                        </div>
+                        {
+                            items.map((data,indx)=>{
+
+                       return(
+                        <div className='eachItem' key={indx}>
+                        <h4 className='heading'>{data}</h4>
+                        <i className="fa fa-trash del-btn " onClick={()=>deleteItem(indx)}></i>
+                    </div>
+                       )
+                            })
+                        }
+                
 
                     </div> 
                     <div className='clearItems'>
